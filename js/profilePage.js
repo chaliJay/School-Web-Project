72a96f7df841e57432a044aa88ff3b947fb46031
wyatt025 <wnickerso@gmail.com>
2026-04-22 15:52:57 +0300

    added username to profile page

--- js/profilePage.js
+++ js/profilePage.js
@@ -4,23 +4,22 @@ const user = JSON.parse(localStorage.getItem("user"));
 const userId = localStorage.getItem("userId");
 
 console.log("User from localStorage:", user);
-console.log("UserId:", userId);
+//console.log("UserId:", userId);
 
 if (!user || !userId) {
   alert("User not logged in properly");
   window.location.href = "login.html";
 }
 
+document.getElementById("username").textContent = user.username;
 document.getElementById("name").textContent = user.fullname;
 document.getElementById("email").textContent = user.email;
-
 document.getElementById("gender").textContent = user.gender;
 document.getElementById("birthday").textContent = user.birthday;
 
-
 fetch(`${BACKEND_ROOT_URL}/profile/${userId}`)
   .then(res => {
-    console.log("STATUS:", res.status);
+    //console.log("STATUS:", res.status);
 
     return res.json();
   })
@@ -37,4 +36,7 @@ document.getElementById("birthday").textContent = formattedDate;
   .catch(err => {
     console.error("❌ FETCH ERROR:", err);
   });
-  console.log(user)
+  //console.log(user)
+  async function EditClick(id){
+
+  }
