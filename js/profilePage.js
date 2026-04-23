39b1d30d781febe29272a3f4595a407d2aff2a03
wyatt025 <wnickerso@gmail.com>
2026-04-23 14:39:59 +0300

    Profile page update

--- js/profilePage.js
+++ js/profilePage.js
@@ -12,38 +12,20 @@ if (!user || !userId) {
 }
 
 async function loadProfileDetails() {
-    fetch(`${BACKEND_ROOT_URL}/profile/${userId}`)
-    .then(res => res.json())
-    .then(data => {
-        document.getElementById("username").textContent = data.username;
-        document.getElementById("name").textContent = data.fullname;
-        document.getElementById("email").textContent = data.email;
-        document.getElementById("gender").textContent = data.gender;
-        
-    });
+  try {
+      const response = await fetch(`${BACKEND_ROOT_URL}/profile/${userId}`);
+      const data = await response.json();
+      document.getElementById("username").textContent = data.username;
+      document.getElementById("name").textContent = data.fullname;
+      document.getElementById("email").textContent = data.email;
+      document.getElementById("gender").textContent = data.gender ? data.gender : "Not available";
+      document.getElementById("birthday").textContent = data.birthday ? data.birthday.split("T")[0] : "Not set";
+    } 
+    catch (error) {
+    console.error("Error fetching profile details:", error);
+    }
 }
 loadProfileDetails();
-document.getElementById("birthday").textContent = user.birthday;
-
-fetch(`${BACKEND_ROOT_URL}/profile/${userId}`)
-  .then(res => {
-    //console.log("STATUS:", res.status);
-
-    return res.json();
-  })
-  .then(data => {
-    document.getElementById("gender").textContent =
-      data.gender ? data.gender : "Not available";
-
-    const formattedDate = data.birthday
-  ? data.birthday.split("T")[0]
-  : "Not set";
-
-document.getElementById("birthday").textContent = formattedDate;
-  })
-  .catch(err => {
-    console.error("❌ FETCH ERROR:", err);
-  });
   //console.log(user)
   async function EditClick(id){
 
